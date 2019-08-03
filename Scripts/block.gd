9af8f784ddd3a2d4b03cbe543362ebe46ceebf1f
antoine giego <viessur@discord.com>
2019-08-03 18:22:10 +0200

    player can chose where to put the blocks

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -4,9 +4,39 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
+
+onready var animator = get_node("AnimationPlayer")
+var blocks = ["cube", "L", "bar"]
+
+var paused = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func pause():
+	paused = true
+	change_sleep(paused)
+
+func resume():
+	paused = false
+	change_sleep(paused)
+
+func change_sleep(value):
+	for block in blocks :
+		get_node(block).sleeping=value
+
+func random_block():
+	randomize()
+	return blocks[randi()%len(blocks)]
+
+func set_block(value):
+	animator.play(value)
+
+func set_random_block():
+	var rand = random_block()
+	print(rand)
+	animator.play(rand)
 
 func _physics_process(delta):
 	pass
--- Scripts/lego game.gd
+++ Scripts/lego game.gd
@@ -9,41 +9,91 @@ onready var path = get_node("Path")
 onready var generator = path.curve
 onready var timer = get_node("Timer")
 onready var camera = get_node("Camera")
+onready var selection_block = get_node("block")
 
 var paused
+var blocks
+var length
+var positions
+
+var idx_positions = 0
+var current_selection = "cube"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	paused = false
+	paused = true
+	length = generator.get_baked_length()
+	positions = range(0,length,length/5)
+	change_selection(current_selection)
+	move_selection()
+
+func move_selection():
+	var t = positions[idx_positions]/length
+	selection_block.transform.origin = generator.interpolate(0,t) + path.transform.origin
+
+func change_selection(value):
+	current_selection = value
+	selection_block.animator.play("selection_"+current_selection)
 
 func _process(delta):
-	pass
+	if !paused :
+		if Input.is_action_just_pressed("ui_right") :
+			idx_positions += 1 
+			idx_positions %= len(positions)
+		if Input.is_action_just_pressed("ui_left"):
+			idx_positions -= 1 
+			idx_positions %= len(positions)
+		move_selection()
+		if Input.is_action_just_pressed("ui_up"):
+			if current_selection == "cube" :
+				current_selection = "L"
+			elif current_selection == "L":
+				current_selection = "bar"
+			elif current_selection == "bar":
+				current_selection = "cube"
+		if Input.is_action_just_pressed("ui_down"):
+			if current_selection == "cube" :
+				current_selection = "bar"
+			elif current_selection == "bar":
+				current_selection = "L"
+			elif current_selection == "L":
+				current_selection = "cube"
+		change_selection(current_selection)
+		if Input.is_action_just_pressed("jump"):
+			spawn_block(selection_block.transform.origin - path.transform.origin, true)
 
 func pause():
 	paused = true
 	timer.set_paused(paused)
 	for child in path.get_children():
-		child.sleeping = true
+		child.pause()
 
 func resume():
 	paused = false
 	timer.set_paused(paused)
 	for child in path.get_children():
-		child.sleeping = false
+		child.resume()
 
 func start():
 	print("started lego game !")
 	timer.start()
+	resume()
 
 func abandon():
 	timer.stop()
+	paused = true
 
-func spawn_block(position):
+func spawn_block(position, selected = false):
 	print("spawn block !")
 	var block = Block.instance()
 	block.transform.origin = position
 	path.add_child(block)
+	if selected :
+		block.set_block(current_selection)
+	else :
+		block.set_random_block()
 
 func _on_Timer_timeout():
 	randomize()
-	spawn_block(generator.interpolate(0,randf()))
+	var t = positions[randi()%len(positions)]/length
+	#spawn_block(generator.interpolate(0,t))
